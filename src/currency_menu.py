f54624f1e276b98720575b502bb59094a395626d
Frankelly Cordero <frankellycorderoe@gmail.com>
2026-03-28 19:38:27 -0400

    REFACTORED: menu class logic to support responsive resizing

--- src/currency_menu.py
+++ src/currency_menu.py
@@ -2,18 +2,17 @@ import curses
 
 
 class Menu:
-    def __init__(self, content: str, colors: list[int], nlines: int, ncols: int, beginY: int, beginX: int) -> None:
+    def __init__(self, content: str, colors: list[int]) -> None:
         self.content: str = content
         self.colors: list[int] = colors
-        self.nlines: int = nlines
-        self.ncols: int = ncols
-        self.beginY: int = beginY
-        self.beginX: int = beginX
-        
+
+    def make_menu_window(self, nlines, ncols, beginY, beginX):        
+
         self.menuscr: curses.window = curses.newwin(nlines, ncols, beginY, beginX)
         self.height, self.width = self.menuscr.getmaxyx()
         self.contentX: int = max(0, (self.width - len(self.content)) // 2)
         self.contentY: int = self.height//2 
+        self.draw_menu()
 
     def draw_menu(self):
         self.menuscr.erase()
